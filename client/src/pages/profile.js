c82e17e560c687e39a7da21f11b18b60a4c6befa
Robert Ramirez <metaknight757@gmail.com>
2019-08-13 14:38:28 -0500

    Readme is ready

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -4,7 +4,7 @@ import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 import API from "../utils/API";
-import { Input, TextArea, FormBtn } from "../components/Form";
+
 
 
 
