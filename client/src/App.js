9ce1c76a2327803e6bf4b110cf71c1ad316bc7ac
Aaron Nealy <50381328+aaronnealy@users.noreply.github.com>
2019-08-13 14:34:53 -0500

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/home";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/login" component={Login} />
           <Route exact path="/profile" component={Profile} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
