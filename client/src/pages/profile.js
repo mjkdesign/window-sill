efbfeaef4323db38bb58804e36a8030b164335c6
hausu <kennedymj2@gmail.com>
2019-08-13 09:46:24 -0500

    working on profile functionality

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -38,6 +38,8 @@ class Profile extends Component {
         return (
           <Container fluid>
             <Row>
+              <Col size="md-6">
+              <h1>Available Plants</h1>
               {this.state.plants.length ? (
               <List>
                 {this.state.plants.map(plant => (
@@ -54,7 +56,30 @@ class Profile extends Component {
               <h3>No Plants :^</h3>
             )
             }
-
+              <h1>Add a Plant</h1>
+              <form>
+                <Input 
+                  value={this.state.title}
+                  onChange={this.handleInputChange}
+                  name="title"
+                  placeholder="Plant Type (required)"
+                />
+                <Input 
+                  value={this.state.light}
+                  onChange={this.handleInputChange}
+                  name="light"
+                  placeholder="Light requirement"
+                />
+                <Input 
+                  value={this.state.water}
+                  onChange={this.handleInputChange}
+                  name="water"
+                  placeholder="water requirement"
+                />
+              </form>
+              </Col>
+              <Col size="md-6">
+              </Col>
             </Row>
           </Container>
         );
