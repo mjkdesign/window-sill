1f375b70e36e0d6f19611109f3ae13f81cdeb81b
hausu <kennedymj2@gmail.com>
2019-08-13 10:35:32 -0500

    post route written to database

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -13,8 +13,8 @@ export default {
 //   deleteBook: function(id) {
 //     return axios.delete("/api/books/" + id);
 //   },
-  // Saves a book to the database
-//   saveBook: function(bookData) {
-//     return axios.post("/api/books", bookData);
-//   }
+  // Saves a plant to the database
+  savePlant: function(plantData) {
+    return axios.post("/api/plants", plantData);
+  }
 };
